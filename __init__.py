1fb8ecdb5e06f68042dea5dcd8a6c4b89ddcb2d0
Mario Frasca <mariotomo@gmail.com>
2012-09-08 09:23:17 +0200

    imports data from tpl, but mistakenly allows double import.

--- __init__.py
+++ __init__.py
@@ -16,9 +16,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import or_, and_
 
 
-## accueil - reception - index page
+from tpl import query_tpl
+
+
+## root - reception - index page
 @app.route('/')
-def show_accueil():
+def show_root():
     return render_template('index.html')
 
 
@@ -35,7 +38,8 @@ def login():
             else:
                 session['logged_in'] = user
                 flash('You were logged in')
-                return redirect(url_for('show_accueil'))
+                ## redirect to root page
+                return redirect(url_for('show_root'))
         except:
             error = 'Invalid username'
     ## control flows here if method==GET or any error occurred
@@ -46,7 +50,7 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_accueil'))
+    return redirect(url_for('show_root'))
 
 
 @app.route('/mapping', methods=['GET', 'POST'])
@@ -59,12 +63,74 @@ def the_garden():
     return render_template('accueil.html')
 
 
+class make_pk_functor(object):
+    def __init__(self):
+        self.used = {}
+    def __call__(self, entry):
+        self.used.setdefault(entry, 0)
+        self.used[entry] += 1
+        return ("%s.%d" % (entry, self.used[entry]))[-12:]
+make_pk = make_pk_functor()
+
+
+def plant_insert(genera, genus, species):
+    """allow user to review 'know' and insert 'have/put'.
+
+    `genera` is a list of Taxon objects matching `genus`.
+    `genus` is the user requested string.
+    `species` is the user requested string.
+    """
+
+    ## if user calls twice, should NOT repeat tpl import!!!
+
+    from_tpl = []
+    tpl_to_taxon = {}
+    for item in genera:
+        from_tpl.extend(query_tpl(item.epithet))
+        tpl_to_taxon[item.epithet] = item.pk
+
+    ## from_tpl holds species and subspecies in tpl format
+    for row in from_tpl:
+        ## add species to database, associating parent
+        if row['Infraspecific rank'] == '':
+            parent = row['Genus']
+            child = row['Genus'], row['Species']
+            rank = 'RNK_S0'
+        else:
+            parent = row['Genus'], row['Species']
+            child = row['Genus'], row['Species'], row['Infraspecific epithet']
+            rank = {'subsp.': 'RNK_S2',
+                    'var.': 'RNK_V0',
+                    'f.': 'RNK_F1',
+                    }[row['Infraspecific rank']]
+
+        tpl_to_taxon[child] = make_pk(tpl_to_taxon[parent])
+        g.session.add(Taxon(fk_parent=tpl_to_taxon[parent],
+                          fk_rank=rank,
+                          epithet=row['Species'],
+                          pk=tpl_to_taxon[child]))
+    g.session.commit()
+
+    return render_template('search.html', objects=genera)
+
+
 @app.route('/search', methods=['GET', 'POST'])
 def plant_search():
+    """search the database, show 'know/have/put'.
+
+    if called with 'action'=='insert', invoke `plant_insert`.
+    """
+
+    g.session.rollback()
+
     genus, species = ([i for i in request.form['name'].split(" ") if i] + [''])[:2]
     taxa = g.session.query(Taxon)
     taxa = taxa.filter(and_(Taxon.fk_rank=="RNK_G0",
                             Taxon.epithet.ilike(genus)))
+
+    if request.form['action'] == 'insert':
+        return plant_insert(taxa, genus, species)
+
     if species:
         parent_pk = [i.pk for i in taxa]
         taxa = g.session.query(Taxon)
