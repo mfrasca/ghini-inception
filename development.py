182da9ec5ed2306efbf3ed52d327d068353896c0
Mario Frasca <mrgsfrasca@gmail.com>
2014-04-03 14:41:44 -0500

    cleaning up python remnants.

--- development.py
+++
@@ -1,9 +0,0 @@
-# configuration
-DBHOST = 'localhost'
-DATABASE = 'cuchubo'
-DEBUG = True
-## import os; os.urandom(24)
-SECRET_KEY = 'development key'
-USERNAME = 'mario'
-PASSWORD = 'mario'
-ADMIN = True
--- production.py
+++
@@ -1,9 +0,0 @@
-# configuration
-DBHOST = 'postgresql1.alwaysdata.com'
-DATABASE = 'kruiskruid_cuchubo'
-DEBUG = True
-## import os; os.urandom(24)
-SECRET_KEY = 'development key'
-USERNAME = 'kruiskruid_web'
-PASSWORD = 'silly'
-ADMIN = False
